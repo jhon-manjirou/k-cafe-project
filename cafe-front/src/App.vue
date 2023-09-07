a965d03b6a2af6adc92d3a878a6673cb96f2d5f3
jhon-manjirou <mizoguchi2k@gmail.com>
2023-09-07 14:01:51 +0900

    css修正

--- cafe-front/src/App.vue
+++ cafe-front/src/App.vue
@@ -22,10 +22,10 @@ import { RouterLink, RouterView } from 'vue-router'
     </div>
     <div class="menu" v-show="active">
       <ul>
-        <li><a href="#menu" @click="toggleMenu">メニュー</a></li>
-        <li><a href="#product" @click="toggleMenu">おすすめ</a></li>
-        <li><a href="#company" @click="toggleMenu">ミッション</a></li>
-        <li><a href="#news" @click="toggleMenu">お問い合わせ</a></li>
+        <li><a href="#" @click="toggleMenu">メニュー</a></li>
+        <li><a href="#" @click="toggleMenu">おすすめ</a></li>
+        <li><a href="#" @click="toggleMenu">ミッション</a></li>
+        <li><a href="#" @click="toggleMenu">マッチング</a></li>
       </ul>
     </div>
   </header>
